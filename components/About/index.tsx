e1d0ff3d159b9d600372b0c818c1fc4ffb471abe
Diego Ricardo <diegoricardoweb@gmail.com>
2022-10-02 00:11:15 -0300

    Criado componente About

---
+++ components/About/index.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+
+const About = () => {
+  return (
+    <div className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5]">
+            Sobre
+          </p>
+          <h2 className="py-4">Quem sou eu</h2>
+
+          <p className="py-2 text-gray-600">
+            Eu me especializei na criação de aplicações front-end responsivas
+            para dispositivos móveis que se conectam com APIs e outras
+            tecnologias de back-end. Eu sou apaixonado por aprender novas
+            tecnologias e entender que há mais de uma maneira de realizar uma
+            tarefa. Embora eu seja mais proficiente na construção de aplicações
+            front-end usando HTML, CSS, Javascript e React, eu aprendo rápido e
+            posso pegar novas tecnologias conforme eu precise. Acredito que para
+            ser um grande desenvolvedor não é apenas usar uma linguagem
+            específica, mas sim escolher a melhor ferramenta para o seu
+            trabalho.
+          </p>
+
+          <p className="py-2 text-gray-600">
+            Eu iniciei como desenvolvedor web em 2012, trabalhando com Front-end
+            e Back-end, tenho experiência no CMS Wordpress que já fiz diversas
+            aplicações, já fiz também algumas aplicações utilizando laravel e
+            outras tecnologias.
+          </p>
+
+          <p className="py-2 text-gray-600 underline cursor-pointer">
+            Confira alguns dos meus últimos projetos.
+          </p>
+        </div>
+
+        <div className="w-full h-full m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300 relative">
+          <Image
+            src="/../public/assets/about.jpg"
+            className="rounded-xl"
+            alt="/"
+            layout="fill"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import About from '../components/About'
+import Main from '../components/Main'
 import Navbar from '../components/Navbar'
 
 const Home: NextPage = () => {
@@ -10,6 +12,10 @@ const Home: NextPage = () => {
       </Head>
 
       <Navbar />
+      <main>
+        <Main />
+        <About />
+      </main>
     </div>
   )
 }
