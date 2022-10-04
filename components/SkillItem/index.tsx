3b87de5f5a58c05022ddce69cf6ad09aee558240
Diego Ricardo <diegoricardoweb@gmail.com>
2022-10-03 22:59:42 -0300

    Criado componente SkillItem

---
+++ components/SkillItem/index.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+
+type SkillItemProps = {
+  image: string
+  title: string
+}
+
+const SkillItem = ({ image, title }: SkillItemProps) => {
+  return (
+    <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+      <div className="grid grid-cols-2 gap-4 justify-center items-center">
+        <div className="m-auto">
+          <Image src={image} width="64px" height="64px" alt="Html" />
+        </div>
+
+        <div className="flex flex-col items-center justify-center">
+          <h3>{title}</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SkillItem
