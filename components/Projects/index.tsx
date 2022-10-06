b4c01d91da6fdf3996ab6ab3c42afb188c6e7e0b
Diego Ricardo <diegoricardoweb@gmail.com>
2022-10-05 21:33:49 -0300

    Criado componente Project

---
+++ components/Projects/index.tsx
@@ -0,0 +1,46 @@
+import ProjectItem from '../ProjectItem'
+import propertyImg from '../../public/assets/projects/property.jpg'
+import cryptoImg from '../../public/assets/projects/crypto.jpg'
+import netflixImg from '../../public/assets/projects/netflix.jpg'
+import twitchImg from '../../public/assets/projects/twitch.jpg'
+
+const Projects = () => {
+  return (
+    <div className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Projetos
+        </p>
+        <h2 className="py-4">O que construí</h2>
+
+        <div className="grid md:grid-cols-2 gap-8">
+          <ProjectItem
+            title="Property Finder"
+            backgroundImg={propertyImg}
+            projectUrl="/property"
+          />
+
+          <ProjectItem
+            title="Crypto App"
+            backgroundImg={cryptoImg}
+            projectUrl="/crypto"
+          />
+
+          <ProjectItem
+            title="Netflix"
+            backgroundImg={netflixImg}
+            projectUrl="/netflix"
+          />
+
+          <ProjectItem
+            title="Twitch"
+            backgroundImg={twitchImg}
+            projectUrl="/twitch"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,22 @@ import Head from 'next/head'
 import About from '../components/About'
 import Main from '../components/Main'
 import Navbar from '../components/Navbar'
+import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Diego Portfolio</title>
+        <title>Diego Ricardo - FullStack Developer</title>
       </Head>
 
       <Navbar />
-      <main>
+      <main className="px-[7px] md:px-0">
         <Main />
         <About />
         <Skills />
+        <Projects />
       </main>
     </div>
   )
