401a636557879f5c00b3687f729b1014cea351ec
Diego Ricardo <diegoricardoweb@gmail.com>
2022-10-05 21:33:31 -0300

    Criado componente ProjectItem

--- components/ProjectItem/index.tsx
+++ components/ProjectItem/index.tsx
@@ -27,7 +27,7 @@ const ProjectItem = ({
         </h3>
         <p className="pb-4 pt-2 text-white text-center">React JS</p>
         <Link href={projectUrl}>
-          <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
+          <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer w-[143px] m-auto">
             More Info
           </p>
         </Link>
