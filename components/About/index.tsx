63872fd34ec383b63245ef71ebab1e59866de8dc
Diego Ricardo <diegoricardoweb@gmail.com>
2022-10-09 00:30:25 -0300

    Animando About

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,11 +1,30 @@
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 import aboutImg from '../../public/assets/about.jpg'
 
 const About = () => {
   return (
-    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="w-full md:h-screen p-2 flex items-center py-16"
+    >
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
-        <div className="col-span-2">
+        <motion.div
+          initial={{
+            x: -300,
+            opacity: 0
+          }}
+          transition={{
+            duration: 1
+          }}
+          whileInView={{
+            opacity: 1,
+            x: 0
+          }}
+          className="col-span-2"
+        >
           <p className="uppercase text-xl tracking-widest text-[#5651e5]">
             Sobre
           </p>
@@ -34,13 +53,26 @@ const About = () => {
           <p className="py-2 text-gray-600 underline cursor-pointer">
             Confira alguns dos meus últimos projetos.
           </p>
-        </div>
+        </motion.div>
 
-        <div className="w-full h-full m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300 relative">
+        <motion.div
+          initial={{
+            x: 300,
+            opacity: 0
+          }}
+          transition={{
+            duration: 1
+          }}
+          whileInView={{
+            opacity: 1,
+            x: 0
+          }}
+          className="w-full h-full m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 relative"
+        >
           <Image src={aboutImg} className="rounded-xl" alt="/" />
-        </div>
+        </motion.div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
