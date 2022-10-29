ef23356093b6fc179e69c711814b0fcd7621af1f
Diego Ricardo <diegoricardoweb@gmail.com>
2022-10-28 22:01:58 -0300

    Retirado comentarios do codigo

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -2,10 +2,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
-
 export default function (req: NextApiRequest, res: NextApiResponse) {
   const nodemailer = require('nodemailer')
 
