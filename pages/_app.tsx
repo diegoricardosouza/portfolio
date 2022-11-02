3d77d4194bbb66678460fca485bb33ef894850f2
Diego Ricardo <diegoricardoweb@gmail.com>
2022-11-02 02:03:34 -0300

    Adicionado SEO na pagina global

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+
+import { DefaultSeo } from 'next-seo'
+import SEO from '../next-seo.config'
+
 import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo {...SEO} />
       <Navbar />
 
       <Component {...pageProps} />
