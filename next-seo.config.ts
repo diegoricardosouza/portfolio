a34f0010175af75507a8c8ccc94ed832fa170d0c
Diego Ricardo <diegoricardoweb@gmail.com>
2022-11-02 02:01:26 -0300

    Adicionado Next SEO

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://www.diegoricardoweb.dev',
+    siteName: 'Diego Ricardo - FullStack Developer'
+  },
+  twitter: {
+    handle: '@dricardosouza',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
