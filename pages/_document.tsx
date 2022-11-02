5a798dbad873c53c5fc83fee3c37241e33635740
Diego Ricardo <diegoricardoweb@gmail.com>
2022-11-02 02:38:29 -0300

    Adicionado Analytics

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import Analytics from '../components/Analytics'
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <Analytics />
+      </body>
+    </Html>
+  )
+}
